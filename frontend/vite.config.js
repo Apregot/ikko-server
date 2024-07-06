195dc916ab205c4da20161bc24d0413119d35869
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2024-07-06 09:56:29 +0200

    ikko: добавил базовую обвязку фронтенда

---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	base: './',
+	server: {
+		// host: '192.168.3.197',
+		// port: 5173,
+	},
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
